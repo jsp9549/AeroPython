cd72d8fd178073c1259c209d2cbc243a65135422
jsp9549 <jsp9549@hotmail.com>
2021-04-24 09:42:07 +0200

    Progress up to moment

---
+++ Python_Books/Task.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 21:01:31 2021
+
+@author: juan
+"""
+
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+
+A_x = np.loadtxt('/home/juan/Documents/Aerodynamics_Course_GW/AeroPython/lessons/resources/NACA0012_x.txt')
+A_y = np.loadtxt('/home/juan/Documents/Aerodynamics_Course_GW/AeroPython/lessons/resources/NACA0012_y.txt')
